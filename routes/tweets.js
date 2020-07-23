b6dec35ead0df09fe2ccc3937e22daaa72c3cf25
nattababi <nataly.babicheva@gmail.com>
2020-07-23 18:18:44 -0400

    test filtering

--- routes/tweets.js
+++ routes/tweets.js
@@ -9,8 +9,14 @@ router.get("/", async (req, res) => {
   let tweets = await httpTwitterService.get(
     '/search/tweets.json?q=from:realDonaldTrump&result_type=mixed&tweet_mode=extended&sort_by=created_at-desc'
   );
+
+
   
   shortTweets = tweets.data.statuses.map(element => ({submitted_at: element.created_at, message: element.full_text}));
+  shortTweets.map(x => {
+    x.submitted_at = x.submitted_at.replace('Wed', 'Fri');
+    return x;
+  });
 
   //console.log(shortTweets);
 
