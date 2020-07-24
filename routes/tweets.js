91c7049877690d86a84998047f3665050024cf50
nattababi <nataly.babicheva@gmail.com>
2020-07-24 15:45:22 -0400

    sorting using moment.js library

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,4 +1,3 @@
-//import axios from "axios";
 const moment = require("moment");
 const express = require("express");
 const router = express.Router();
@@ -10,17 +9,13 @@ router.get("/", async (req, res) => {
     '/search/tweets.json?q=from:realDonaldTrump&result_type=mixed&tweet_mode=extended&sort_by=created_at-desc'
   );
 
-
-  
   shortTweets = tweets.data.statuses.map(element => ({submitted_at: element.created_at, message: element.full_text}));
-  shortTweets.map(x => {
-    x.submitted_at = x.submitted_at.replace('Wed', 'Fri');
-    return x;
+
+  const sortedArray = shortTweets.sort((a, b) => {
+    return moment(b.submitted_at).diff(a.submitted_at);
   });
 
-  //console.log(shortTweets);
-
-  res.send(shortTweets);
+  res.send(sortedArray);
 
 });
 
